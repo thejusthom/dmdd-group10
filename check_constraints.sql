b79482081398abc9e8eca62e3b5be0fc16146641
Thejus Thomson <thomson.th@northeastern.edu>
2024-03-21 02:51:30 -0400

    [Assignment - 3] - Addding check-constraints

---
+++ check_constraints.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION check_constraint_exists (
+    cons_name VARCHAR
+) RETURN NUMBER AS
+    v_exists NUMBER;
+    v_cname  VARCHAR(100);
+BEGIN
+    SELECT
+        COUNT(*)
+    INTO v_exists
+    FROM
+        user_constraints
+    WHERE
+        upper(CONSTRAINT_NAME) = upper(cons_name);
+
+    IF v_exists > 0 THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
+EXCEPTION
+    WHEN OTHERS THEN
+        RETURN 0;
+END check_constraint_exists;
+
