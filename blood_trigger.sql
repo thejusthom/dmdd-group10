e1a8794d029f6efd0460198f2f4c3bebbb0d4f66
PranavPrak <prakash.pran@northeastern.edu>
2024-04-12 15:40:22 -0400

    Updating the isconsumed field when quantity is available

--- blood_trigger.sql
+++ blood_trigger.sql
@@ -21,6 +21,34 @@ BEGIN
         AND dbca.donated_date >= sysdate - 42
         AND dbca.isbloodconsumed = 'N';
 
+    IF v_available_blood >= :new.quantity_required THEN
+        -- Update isbloodconsumed to 'Y' for the specified blood group and other criteria
+        dbms_output.put_line(v_available_blood);
+        -- Update isbloodconsumed to 'Y' for the specified blood group and other criteria
+        MERGE INTO donor_blood_camp_asso dbca
+        USING (
+            SELECT
+                dbca.rowid AS rid,
+                d.donor_id,
+                p.person_id
+            FROM
+                     donor_blood_camp_asso dbca
+                JOIN donor  d ON dbca.donor_donor_id = d.donor_id
+                JOIN person p ON d.person_person_id = p.person_id
+            WHERE
+                    dbca.isbloodconsumed = 'N'
+                AND dbca.donated_date >= sysdate - 42
+                AND ROWNUM <= :new.quantity_required
+        ) src ON ( dbca.rowid = src.rid )
+        WHEN MATCHED THEN UPDATE
+        SET dbca.isbloodconsumed = 'Y';
+        dbms_output.put_line(v_available_blood);
+
+
+    ELSE
+        RAISE blood_error;
+    END IF;
+
 EXCEPTION
     WHEN blood_error THEN
         dbms_output.put_line('Exception in blood triggers, blood quantity unavailable' || sqlerrm);
